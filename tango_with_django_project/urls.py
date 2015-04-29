57f86b44332e416bdc9baa9bab8a47a3eceb8de6
Joshua <lusidium.dev@gmail.com>
2015-04-29 13:20:43 -0400

    Setup Rango view, and url mappings

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,10 +1,7 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, patterns, url
 from django.contrib import admin
 
-urlpatterns = [
-    # Examples:
-    # url(r'^$', 'tango_with_django_project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-]
+urlpatterns = patterns('',
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^rango/', include('rango.urls')),
+)
